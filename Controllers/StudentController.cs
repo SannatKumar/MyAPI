d080fe5f97abf3e25a29ae92052051deac84a7bf
Raj Kumar <interplaytheheads@gmail.com>
2021-02-05 20:43:19 +0200

    Get All Students Function added

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -10,5 +10,29 @@ namespace MyAPI.Controllers
 {
     public class StudentController : ApiController
     {
+
+        //GET -Retrieve data
+
+        public IHttpActionResult GetAllStudent()
+        {
+            IList<StudentDisplay> students = null;
+            using (var x = new WebAPI_PeopleEntities())
+            {
+                students = x.Students
+                    .Select(c => new StudentDisplay()
+                    {
+                        Id = c.id,
+                        Name = c.name,
+                        Email = c.email,
+                        Country = c.country,
+                        Phone = c.phone,
+                        TempAdd = c.tempadd
+                    }).ToList<StudentDisplay>();
+            }
+            if (students.Count == 0)
+                return NotFound();
+            return Ok(students);
+
+        }
     }
 }
