68ef2df90ece684c17de242b11b254b8c1d2d63f
Raj Kumar <interplaytheheads@gmail.com>
2021-02-05 20:28:38 +0200

    Student Model is defined

---
+++ Models/StudentDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyAPI.Models
+{
+    public class StudentDisplay
+    {
+        public int Id { get; set; }
+
+        public String Name { get; set; }
+
+        public String Email { get; set; }
+
+        public String Country { get; set; }
+
+        public String Phone { get; set; }
+
+        public String TempAdd { get; set; }
+    }
+}
